7e578399fdd17a51b758c99c4f54bcadee210d21
ayoung-iya <rhaehfdl1879@naver.com>
2023-10-15 22:28:26 +0900

    시간영역 구현 분까지만 구현하는데, 59초에 화면을 띄웠을 때, 1초뒤에 시간이 변해야 한다는 생각에 setInterval 시간간격을 1초로 했는데 괜찮은지는 모르겠다.

---
+++ js/clock.js
@@ -0,0 +1,19 @@
+const days = ['일', '월', '화', '수', '목', '금', '토'];
+
+const clockGroup = document.querySelector(".clock-group");
+
+function current() {
+  const currentHour = new Date().getHours();
+  const currentMinute = new Date().getMinutes();
+  const currentMonth = new Date().getMonth() + 1;
+  const currentDate = new Date().getDate();
+  const currentDay = `${days[new Date().getDay()]}요일`;
+  
+  clockGroup.innerHTML = `
+    <span class="clock">${currentHour}:${currentMinute}</span>
+    <span class="date">${currentMonth}월 ${currentDate}일 ${currentDay}</span>
+  `
+}
+
+current();
+setInterval(current, 1000);
