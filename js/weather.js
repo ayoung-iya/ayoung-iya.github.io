22e01a65a8a95754a13aca9323c197ff42c7dcad
ayoung-iya <rhaehfdl1879@naver.com>
2023-10-16 01:17:05 +0900

    날씨 정보 적용해보기

--- js/weather.js
+++ js/weather.js
@@ -1,12 +1,30 @@
 function onGeoOk(position) {
+  const weatherArea = document.querySelector('.weather-area');
+  
   const API_KEY = "91e9f3878adf25f8741778736757e512";
 
   const lat = position.coords.latitude;
   const long = position.coords.longitude;
 
-  fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${API_KEY}`)
+  fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${API_KEY}&lang=kr&units=metric`)
     .then(response => response.json())
     .then(data => {
+      const region = data.name;
+      const currentTemp = data.main.temp;
+      const maxTemp = data.main.temp_max;
+      const minTemp = data.main.temp_min;
+      const weather = data.weather[0].description;
+
+      weatherArea.innerHTML = `
+      <p class="weather-group">
+        <span class="temperature">${currentTemp}°</span>
+        <span class="region">${region} ${weather}</span>
+      </p>
+      <p class="temperature-group">
+        <span class="high">최고: ${maxTemp}°</span>
+        <span class="low">최저: ${minTemp}°</span>
+      </p>
+      `
       console.log(data);
     })
 }
